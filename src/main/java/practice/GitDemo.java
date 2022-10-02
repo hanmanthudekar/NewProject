aee344b6ae1beb26d3072b506ba0e1e99283bf5b
hanmanthudekar <hanmanthudekar@gmail.com>
2022-10-02 11:21:02 +0530

    New Project

---
+++ src/main/java/practice/GitDemo.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hii");
+	}
+}
---
+++ src/main/java/practice/NewClass.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New class created.");
+	}
+}
